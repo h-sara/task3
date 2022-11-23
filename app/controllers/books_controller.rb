aa4a9829655afc5c246300ea0c6ada6784605fab
h-sara <115357536+h-sara@users.noreply.github.com>
2022-11-23 06:55:16 +0000

    [Break] task3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,44 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.new #New bookの所の@book
+    @user = current_user #User infoの所の@user
+    @booker = Book.find(params[:id]) #Book detailで使用
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id #redirect_to book_path(@book.id)の際に必要
+    @book.save
+    @user = current_user #User infoの所の@user
+    # @books = Book.all
+    redirect_to book_path(@book.id), notice: "You have created book successfully."
   end
 
   def index
+    @book = Book.new #New book,Book detailの所の@book
+    @books = Book.all
+    @user = current_user #User infoの所の@user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "You have updated book successfully."
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,39 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id]) #User infoの所の@user
+    @books = @user.books
+    @book_user = Book.new
+  end
+
+  def index
+    @book = Book.new #New book,Book detailの所の@book
+    @books = Book.all
+    @user = current_user #User infoの所の@user
+    @users = User.all #Usersの所の@users
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    @user = current_user #User infoの所の@user
+    # @books = Book.all
+    redirect_to book_path(@book.id)
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id), notice: "You have updated user successfully."
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
